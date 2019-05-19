91c4bf291284b1f9785bcbf03c6eb11b3ee140a0
Mark Wu <mark14@mail.ustc.edu.cn>
2019-05-19 13:50:28 +0800

    svm fully functional

--- lab02/svm.py
+++ lab02/svm.py
@@ -1,29 +1,68 @@
-from sklearn import svm
-from sklearn.pipeline import Pipeline
+import warnings
 
-from preprocessing import DEAP, MAHNOB_HCI
-
-rbf_svc = svm.SVC(C=1.0, kernel='rbf', loss="hinge")
-
-svm_clf = Pipeline([()])
-from sklearn import datasets
 import numpy as np
+from numpy.core.multiarray import ndarray
+from sklearn.svm import SVC
+from tqdm import tqdm
 
-from sklearn.preprocessing import StandardScaler
+from preprocessing import getDataDEAP, getDataMAHNOB_HCI
 
-iris = datasets.load_iris()
-X = iris["data"][:, (2, 3)]
-y = (iris["target"] == 2).astype(np.float64)
-svm_clf = Pipeline([
-    ("scaler", StandardScaler()),
-    ("linear_svc", LinearSVC(C=1, loss="hinge", random_state=42)),
-])
-svm_clf.fit(X, y)
-svm_clf.predict([[5.5, 1.7]])
+warnings.filterwarnings("ignore")
 
-out:array([1.])
----------------------
-作者：菜鸟知识搬运工
-来源：CSDN
-原文：https: // blog.csdn.net / qq_30815237 / article / details / 88251342
-版权声明：本文为博主原创文章，转载请附上博文链接！
+
+def svm_tuning(C_start, C_end, step):
+    def train_model():
+        score_list = {}
+        for current_C in tqdm(C_list[1:]):
+            rbf_svc = SVC(C=current_C, kernel='rbf')
+            current_scores = []
+            for train, test in indices:
+                rbf_svc.fit(samples[train], labels[train])
+                current_scores.append(float(rbf_svc.score(samples[test], labels[test])))
+            current_score = sum(current_scores) / len(current_scores)
+            score_list[current_C] = current_score
+        return score_list
+
+    C_list: ndarray = np.arange(C_start, C_end, step)
+
+    samples, labels, indices = getDataDEAP()
+    print("Working on DEAP dataset")
+    deap_result = train_model()
+    samples, labels, indices = getDataMAHNOB_HCI()
+    print("Working on MAHNOB-HCI dataset")
+    mahnob_result = train_model()
+
+    max_C = 0
+    max_score = 0
+    for C in C_list[1:]:
+        current_score = deap_result[C] * mahnob_result[C]
+        if current_score > max_score:
+            max_score = current_score
+            max_C = C
+    print("max_c:%f, max_score:%f" % (max_C, max_score))
+
+
+def svm_test(C, getData):
+    """
+
+    :rtype: None
+    """
+    # test on DEAP
+    # X_train, y_train, X_test, y_test, label_encoder = getDataDEAP()
+    samples, labels, indices = getData()
+    scores = []
+    svc = SVC(C=C, kernel='rbf')
+    for train, test in indices:
+        svc.fit(samples[train], labels[train])
+        scores.append(float(svc.score(samples[test], labels[test])))
+    print("score: %f" % (sum(scores) / len(scores)))
+
+
+# svm_tuning(0, 50, 0.01)
+# svm_tuning(0, 5, 1)
+
+C = 0.81
+print("test on DEAP!")
+svm_test(C, getDataDEAP)
+print("test on MAHNOB-HCI!")
+svm_test(C, getDataMAHNOB_HCI)
