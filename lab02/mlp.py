62199240d286c802eda556a114f5c05be7581a89
Mark Wu <mark14@mail.ustc.edu.cn>
2019-05-20 01:13:27 +0800

    add call back for MLP

--- lab02/mlp.py
+++ lab02/mlp.py
@@ -11,19 +11,24 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
 
-from preprocessing import getDataDEAP, getDataMAHNOB_HCI, getNBDataMAHNOB_HCI, getNBDataDEAP
+from preprocessing import getDataDEAP, getDataMAHNOB_HCI, getNBDataMAHNOB_HCI, \
+    getNBDataDEAP, getEmotionDataMAHNOB_HCI
+
 
 
 def train_MLP(x_train, y_train, x_test, y_test):
-    model = Sequential()
     input_size = len(x_train[0])
     output_size = len(y_train[0])
-    model.add(Dense(input_size, activation='relu', input_dim=input_size))
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_dim=input_size))
     model.add(Dropout(0.5))
-    model.add(Dense(15, activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(output_size, activation='softmax'))
+
+    earlystop = [EarlyStopping(patience=4)]
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
@@ -31,9 +36,10 @@ def train_MLP(x_train, y_train, x_test, y_test):
                   metrics=['accuracy'])
 
     model.fit(x_train, y_train,
-              epochs=200,
+              epochs=1000,
               batch_size=128,
-              verbose=0)
+              verbose=0,
+              callbacks=earlystop)
 
     return model.evaluate(x_test, y_test, batch_size=128)
 
@@ -66,8 +72,10 @@ DEAP_score = MLP_classification(getDataDEAP)
 MAHNOB_score = MLP_classification(getDataMAHNOB_HCI)
 DEAP_id_score = MLP_classification(getDataDEAP, getNBDataDEAP)
 MAHNOB_id_score = MLP_classification(getDataMAHNOB_HCI, getNBDataMAHNOB_HCI)
+MAHNOB_emotion_score = MLP_classification(getEmotionDataMAHNOB_HCI)
 
 print("DEAP valence arousal score:", DEAP_score)
 print("MAHNOB valence arousal score:", MAHNOB_score)
 print("DEAP subject id score:", DEAP_id_score)
 print("MAHNOB subject id score:", MAHNOB_id_score)
+print("MAHNOB emotion score:", MAHNOB_emotion_score)
